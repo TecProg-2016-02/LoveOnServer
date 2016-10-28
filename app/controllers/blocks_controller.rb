f698b7c68611a4edbbedea18b5d4b2c54c523a96
Stefania Bezerra da Silva <stebezerra@hotmail.com>
2016-10-28 18:21:50 -0200

    Comment clocks controller

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,13 +1,17 @@
 class BlocksController < ApplicationController
+
   def create
-    block = Block.create(:user_one_id =>params[:user_one_id],
+    #This part of code permit the user block another user
+    block = Block.create(:user_one_id => params[:user_one_id],
       :user_two_id => params[:user_two_id])
     if block.save
       render json: block
     end
   end
+
   def destroy
-    block = Block.where(:user_one_id =>params[:user_one_id],
+    #This part of code permit the user unlock another user
+    block = Block.where(:user_one_id => params[:user_one_id],
       :user_two_id => params[:user_two_id]).first
     block.delete
   end
