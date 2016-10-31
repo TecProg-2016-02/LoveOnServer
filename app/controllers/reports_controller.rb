5c0a4d102c844a554c0a3a0f0967433e2c516615
Stefania Bezerra da Silva <stebezerra@hotmail.com>
2016-10-31 09:45:31 -0200

    Comment report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,11 @@
 class ReportsController < ApplicationController
 
+  # this bloc of code will report an user
   def create
     report = Report.create(:reporter_id =>params[:reporter_id],
       :reported_id => params[:reported_id], :comment => params[:comment])
 
+    # if the report is valid, it will be saved and render in json format
     if report.save
       render json: report
     end
