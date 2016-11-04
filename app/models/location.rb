0d789e185641edd30fe51cdf0965d2e2d719c47f
Gabriel Viana <gabrielv14@gmail.com>
2016-11-04 04:29:11 -0200

    set Location methods as private

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,21 +9,25 @@ class Location < ActiveRecord::Base
                    :lat_column_name => :latitude,
                    :lng_column_name => :longitude
 
-  def generate_token(column)
-    begin
-      self[column] = SecureRandom.urlsafe_base64
-    end while User.exists?(column => self[column])
-  end
+  private
+    def generate_token(column)
+      begin
+        self[column] = SecureRandom.urlsafe_base64
+      end while User.exists?(column => self[column])
+    end
 
-  def male_users(age)
-    self.users.where(:gender => 'male').where(["age <= ?", age])
-  end
+  private
+    def male_users(age)
+      self.users.where(:gender => 'male').where(["age <= ?", age])
+    end
 
-  def female_users(age)
-    self.users.where(:gender => 'female').where(["age <= ?", age])
-  end
+  private
+    def female_users(age)
+      self.users.where(:gender => 'female').where(["age <= ?", age])
+    end
 
-  def all_users(age)
-    self.users.where(:gender => 'female').where(["age <= ?", age])
-  end
+  private
+    def all_users(age)
+      self.users.where(:gender => 'female').where(["age <= ?", age])
+    end
 end
