21d530bc6678f6834a5cef82d8e5106f3efabcf1
Gabriel Viana <gabrielv14@gmail.com>
2016-11-04 04:27:39 -0200

    set Interaction methods as private

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -6,28 +6,31 @@ class Interaction < ActiveRecord::Base
   validate :cant_interact_myself
   before_save :check_match
 
-  def cant_interact_myself
-    if self.user_one.id == self.user_two.id
-      errors.add(:expiration_date, "can't interact with myself")
-    else
-      render :nothing
-    end
-  end
-
-  def match
-    user_match = Match.where(user_one_id: self.user_one.id, user_two_id: self.user_two.id)
-    user_match.first
-  end
-
-  def check_match
-    user_matched = Interaction.where(user_one: self.user_two, user_two:self.user_one)
-    unless user_matched.empty?
-      if user_mathced.first.like && self.like
-        self.matched = true
-        Match.create(user_one_id: self.user_one.id, user_two_id: self.user_two.id)
+  private
+    def cant_interact_myself
+      if self.user_one.id == self.user_two.id
+        errors.add(:expiration_date, "can't interact with myself")
       else
         render :nothing
       end
     end
-  end
+
+  private
+    def match
+      user_match = Match.where(user_one_id: self.user_one.id, user_two_id: self.user_two.id)
+      user_match.first
+    end
+
+  private
+    def check_match
+      user_matched = Interaction.where(user_one: self.user_two, user_two:self.user_one)
+      unless user_matched.empty?
+        if user_mathced.first.like && self.like
+          self.matched = true
+          Match.create(user_one_id: self.user_one.id, user_two_id: self.user_two.id)
+        else
+          render :nothing
+        end
+      end
+    end
 end
