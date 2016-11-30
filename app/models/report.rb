d6954d060101f35bec8a83dff8e3701bd4856544
Stefania Bezerra da Silva <stebezerra@hotmail.com>
2016-11-30 20:52:08 -0200

    commenting  report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -13,17 +13,28 @@ class Report < ActiveRecord::Base
     maximum: 140
   }
 
+# This function will permit taht you can't report yourself
   def cant_report_myself
+    # This line will check if the id that you want report is equal of yours id
     if self.reporter.id == self.reported.id
+      # This is a message that show if you want report yourself
       errors.add(:expiration_date, "can't report myself")
+    # Will render nothing if you dont fit on this
+    else
+      render :nothing
     end
   end
 
   def check_maximum_reports
+    # Will check the reports of the user
     user_report = Report.where(reported:self.reported)
+    # Will check how many reports has been made
     unless user_report.empty?
+      # Will check how many reports that user has
       if user_report.length >= 5
+        # If the user fit in, he will be blocked
         self.reported.block_account
+      # Nothing will happen if they don't fit
       else
         render :nothing
       end
