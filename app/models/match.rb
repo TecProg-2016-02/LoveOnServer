a7fe4eb7c98535b5cf2ebf46716a00f5d5d0cccd
Stefania Bezerra da Silva <stebezerra@hotmail.com>
2016-12-03 16:11:02 -0200

    Comment match.rb

--- app/models/match.rb
+++ app/models/match.rb
@@ -7,12 +7,15 @@ class Match < ActiveRecord::Base
 
   private
     def generate_token(column)
+      # This part will select a person by using base 64 to make this selection
       begin
         self[column] = SecureRandom.urlsafe_base64
-      end while User.exists?(column => self[column])
+      end
+        while User.exists?(column => self[column])
     end
 
   private
+    # This part will check if it is interacting with the same person
     def get_user(id)
       if self.first_user_interaction_id == id
         return self.user_two
